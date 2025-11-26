381c612b292020ffc3715f484c45469301c44298
femgee <femgee@gmail.com>
2025-11-26 19:13:30 +0000

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { KnowledgeBase } from './pages/KnowledgeBase';
+import { Blogs } from './pages/Blogs';
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const handleNavigate = (page: string) => {
+    setCurrentPage(page);
+    setIsSearchOpen(false);
+  };
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home onNavigate={handleNavigate} />;
+      case 'about':
+        return <About onNavigate={handleNavigate} />;
+      case 'knowledge-base':
+        return (
+          <KnowledgeBase
+            onSearchClick={() => setIsSearchOpen(true)}
+            isSearchOpen={isSearchOpen}
+            onSearchClose={() => setIsSearchOpen(false)}
+            onNavigate={handleNavigate}
+          />
+        );
+      case 'blogs':
+        return <Blogs onNavigate={handleNavigate} />;
+      default:
+        return <Home onNavigate={handleNavigate} />;
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header
+        onSearchClick={() => setIsSearchOpen(true)}
+        currentPage={currentPage}
+        onNavigate={handleNavigate}
+      />
+      {renderPage()}
+    </div>
+  );
+}
+
+export default App;
